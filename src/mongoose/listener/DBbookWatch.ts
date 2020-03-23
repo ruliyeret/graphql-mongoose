4b0c54d1671e184d6e30ba87fee21b9ded2d8562
ruli <yerer.rul@gmail,com>
2020-03-23 15:35:32 +0200

    nothing for commit

--- src/mongoose/listener/DBbookWatch.ts
+++ src/mongoose/listener/DBbookWatch.ts
@@ -3,7 +3,7 @@ import {pubsub} from "../../index";
 
 export const DBbookListenr = () =>{
     DbBook.watch().on("change", (data) =>{
-            pubsub.publish("newBook",{"addBook": data.fullDocument});
+            pubsub.publish("newBook ",{"addBook": data.fullDocument});
         console.log("recognize insert into db: " + data.ns.db +
             " collection: " + data.ns.coll +
             " entity: " + JSON.stringify(data.fullDocument));
